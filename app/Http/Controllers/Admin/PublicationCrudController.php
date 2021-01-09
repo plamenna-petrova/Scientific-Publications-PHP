c5e1e62b1a690b3f1346649cc0757b4c169f78fe
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2021-01-09 21:27:51 +0200

    Applied getFieldsData() function to PublicationCrudController

--- app/Http/Controllers/Admin/PublicationCrudController.php
+++ app/Http/Controllers/Admin/PublicationCrudController.php
@@ -19,9 +19,34 @@ class PublicationCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
+    private function getFieldsData($show = FALSE)
+    {
+        return [
+            [
+                'name' => 'title',
+                'label' => 'Title',
+                'type' => 'text'
+            ],
+            [
+                'name' => 'excerpt',
+                'label' => 'Excerpt',
+                'type' => ($show ? "textarea" : 'ckeditor'),
+            ],
+            [
+                'name' => 'type',
+                'label' => 'Type',
+                'type' => 'select_from_array',
+                'options' => ['Article in a magazine' => 'Article in a magazine', 'Report of a conference' => 'Report of a conference', 'Book' => 'Book'],
+                'allow_null' => false,
+                'allows_multiple' => false,
+                'tab' => 'Choose publication type'
+            ]
+        ];
+    }
+
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -29,11 +54,13 @@ class PublicationCrudController extends CrudController
         CRUD::setModel(\App\Models\Publication::class);
         CRUD::setRoute(config('backpack.base.route_prefix') . '/publication');
         CRUD::setEntityNameStrings('publication', 'publications');
+
+        $this->crud->addFields($this->getFieldsData());
     }
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
@@ -44,13 +71,13 @@ class PublicationCrudController extends CrudController
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -63,13 +90,13 @@ class PublicationCrudController extends CrudController
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
