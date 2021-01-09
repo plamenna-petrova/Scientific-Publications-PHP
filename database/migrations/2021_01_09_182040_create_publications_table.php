7c0644789ae9058f19c5cfb6807f53c3223a78b7
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2021-01-09 20:35:13 +0200

    Created Publications Table and added CRUD options with Backpack

---
+++ database/migrations/2021_01_09_182040_create_publications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->unique();
+            $table->longText('excerpt');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publications');
+    }
+}
