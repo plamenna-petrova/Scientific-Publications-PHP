9d1a726c642e174699d53a962dd556d3066a1a7a
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2021-01-10 16:59:33 +0200

    Added Authors and Publications Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,38 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        //Seeding the database
+        \DB::table('authors')->insert([
+            [
+                'name' => 'Diane Brody',
+                'occupation' => 'Software Architect, Freelancer',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Andrew Quentin',
+                'occupation' => 'Cognitive Scientist',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]
+        ]);
+
+        \DB::table('publications')->insert([
+           [
+               'title' => 'AI Consciousness',
+               'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+               'type' => 'Book',
+               'created_at' => date('Y-m-d H:i:s'),
+               'updated_at' => date('Y-m-d H:i:s')
+           ],
+           [
+               'title' => 'Software Architecture for experts',
+               'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+               'type' => 'Report of a conference',
+               'created_at' => date('Y-m-d H:i:s'),
+               'updated_at' => date('Y-m-d H:i:s')
+           ]
+        ]);
     }
 }
