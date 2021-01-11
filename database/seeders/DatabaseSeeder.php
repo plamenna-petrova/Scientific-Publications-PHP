4c109120e6f125726f001e4d8629f3b7393783eb
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2021-01-11 19:29:59 +0200

    Updated Database Seed to include seed for publication_authors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,19 +33,30 @@ class DatabaseSeeder extends Seeder
 
         \DB::table('publications')->insert([
            [
-               'title' => 'AI Consciousness',
+               'title' => 'AI Consciousness Level',
                'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                'type' => 'Book',
                'created_at' => date('Y-m-d H:i:s'),
                'updated_at' => date('Y-m-d H:i:s')
            ],
            [
-               'title' => 'Software Architecture for experts',
+               'title' => 'Software Architecture for the experienced',
                'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                'type' => 'Report of a conference',
                'created_at' => date('Y-m-d H:i:s'),
                'updated_at' => date('Y-m-d H:i:s')
            ]
+        ]);
+
+        \DB::table('publication_authors')->insert([
+            [
+                'publication_id' => 1,
+                'author_id' => 2
+            ],
+            [
+               'publication_id' => 2,
+               'author_id' => 1
+            ]
         ]);
     }
 }
